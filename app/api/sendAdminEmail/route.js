9fbdd08fb54a63f6579adf7fbd50a08413464b5e
Anzi <djam4343@gmail.com>
2025-07-19 19:08:03 +0500

    aded emails

--- app/api/sendAdminEmail/route.js
+++ app/api/sendAdminEmail/route.js
@@ -26,7 +26,7 @@ export async function POST(req) {
 
     const mailOptions = {
       from: `"Your Store" <mehrandadbeh.anziandco@gmail.com>`,
-      to: ["anziandco@gmail.com", "djam4343@gmail.com"],
+      to: ["rajaf6719@gmail.com", "djam4343@gmail.com", "rajasalmanzs83@gmail.com"],
       subject: `New Order Received`,
       text: `A new order has been placed. on your website`,
     };
